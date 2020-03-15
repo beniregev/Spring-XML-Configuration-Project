0e65e031ed2035bfe5b3e90271b1211fb8dd809f
binyamin.regev <beniregev@gmail.com>
2020-03-15 22:49:16 +0200

    Adding TaskA in application class from application-context

--- src/main/java/MXSpringExerciseApplication.java
+++ src/main/java/MXSpringExerciseApplication.java
@@ -1,11 +1,11 @@
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ImportResource;
+
+import model.TaskA;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class MXSpringExerciseApplication {
     public static void main(String[] args) {
-        FileSystemXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:/config/spring-exercise-config.xml");
-
+        FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("classpath:/config/spring-exercise-config.xml");
+        TaskA taskA = context.getBean("taskA", TaskA.class);
     }
 }
