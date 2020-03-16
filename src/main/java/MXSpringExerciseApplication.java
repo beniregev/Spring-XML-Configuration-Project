28b2de4e28bcab83a5a629812a86c1acb473c12b
Binyamin Regev <beniregev@gmail.com>
2020-03-16 18:33:04 +0200

    Updates

--- src/main/java/MXSpringExerciseApplication.java
+++ src/main/java/MXSpringExerciseApplication.java
@@ -1,11 +1,16 @@
-
 import model.TaskA;
+import model.TaskB;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+//import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class MXSpringExerciseApplication {
     public static void main(String[] args) {
-        FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("classpath:/config/spring-exercise-config.xml");
-        TaskA taskA = context.getBean("taskA", TaskA.class);
+        //FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("classpath:/config/spring-exercise-config.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:/spring-configuration.xml");
+        TaskA taskA =  (TaskA) context.getBean("taskA");
+        System.out.println("TaskA String Property is \"" + taskA.getStringProp());
+        TaskB taskB =  (TaskB) context.getBean("taskB");
+        taskB.displayStrings();
     }
 }
