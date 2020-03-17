575cfef6c8ac7af24da517e041fc6115faf220b4
Binyamin Regev <beniregev@gmail.com>
2020-03-17 17:08:38 +0200

    Updated Java Application

--- src/main/java/MXSpringExerciseApplication.java
+++ src/main/java/MXSpringExerciseApplication.java
@@ -1,5 +1,4 @@
-import model.TaskA;
-import model.TaskB;
+import model.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 //import org.springframework.context.support.FileSystemXmlApplicationContext;
@@ -8,9 +7,33 @@ public class MXSpringExerciseApplication {
     public static void main(String[] args) {
         //FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("classpath:/config/spring-exercise-config.xml");
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:/spring-configuration.xml");
+
+        System.out.println("Task A: ");
         TaskA taskA =  (TaskA) context.getBean("taskA");
-        System.out.println("TaskA String Property is \"" + taskA.getStringProp());
+        System.out.println("\tstring Property is \"" + taskA.getStringProp() + "\"");
+        System.out.println("\tpropertyTaskA is { id: " + taskA.getPropertyTaskA().getId() + ", name: \"" + taskA.getPropertyTaskA().getName() + "\" }");
+
+        System.out.println("\nTask B: ");
         TaskB taskB =  (TaskB) context.getBean("taskB");
         taskB.displayStrings();
+
+        System.out.println("\nTask C: ");
+        TaskC taskC = (TaskC) context.getBean("taskC");
+        taskC.displayProperties();
+
+        System.out.println("\nTask D: ");
+        TaskD taskD = (TaskD) context.getBean("taskD");
+        taskD.displayMap();
+
+        System.out.println("\nTask E: ");
+        TaskE taskE = (TaskE) context.getBean("taskE");
+        System.out.println("\tThe message is: " + taskE.getMessage());
+
+        System.out.println("\nTask F: ");
+        TaskF taskF = (TaskF) context.getBean("taskF");
+        taskF.showMessage();
+
+        System.out.println("\nTask G: ");
+
     }
 }
